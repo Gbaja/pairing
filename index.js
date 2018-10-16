4b64172429fc9e5184b477a03151526f2d1d5171
Fatimat Gbajabiamila <gbajaf@yahoo.co.uk>
2018-10-16 14:37:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
+// You have an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
 
 // The lowest number will not always come first.
 
@@ -6,4 +6,6 @@ function sumAll(arr) {
     return 1;
   }
   
-  //sumAll([1, 4]);
+  //sumAll([1, 4]); should return 10
+  //sumAll([10, 5]) should return 45
+  //sumAll([5, 10]) should return 45
